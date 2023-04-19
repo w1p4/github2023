642cda84a1af5681497811c3a3f2bba151bace39
18476391179 <wang-pan-1@163.com>
2023-04-20 03:11:58 +0800

    首次提交前台文件

---
+++ webapp/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+	<h3> {{ var1 }}</h3>
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+	return {
+		var1 : "数据绑定了变量名"
+	}
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
